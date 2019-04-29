9319426f79b1e63b2cc7d258d57ce7dd26ce643f
Roberto <robertopescee@hotmail.com>
2019-04-29 21:31:29 +0200

    Implemented js modules and first event listeners

---
+++ app.js
@@ -0,0 +1,50 @@
+var budgetController = (function() {
+
+	//add code
+
+})();
+
+
+
+
+
+var UIController = (function() {
+
+	//add code
+
+})();
+
+
+
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+	var ctrlAddItem;
+
+	ctrlAddItem = function(){
+
+		//1. Get the field input data
+
+		//2. Add the item to the budget controller
+
+		//3. Add the item to the UI
+
+		//4. Calculate the budget
+
+		//5. Display the budget on the UI
+
+		console.log('it works!');
+
+	};
+
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event){
+		if (event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+	});
+
+})(budgetController, UIController);
