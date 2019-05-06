bdca4917a59debf315ed9d922099b9fdfdd3255e
Roberto <robertopescee@hotmail.com>
2019-05-06 20:39:03 +0200

    Update percentages in budget controller

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ var budgetController = (function() {
 		this.id = id;
 		this.description = description;
 		this.value = value;
+		this.percentage = -1;
+	};
+
+	Expense.prototype.calcPercentages = function(totalIncome) {
+		if (totalIncome > 0) {
+			this.percentage = Math.round((this.value / totalIncome)*100);
+		} else {
+			this.percentage = -1;
+		}
+	};
+
+	Expense.prototype.getPercentage = function() {
+		return this.percentage;
 	};
 
 	var Income = function(id, description, value) {
@@ -89,6 +102,21 @@ var budgetController = (function() {
 			} else {
 				data.percentage = -1;
 			};
+		},
+
+		calculatePercentages: function() {
+
+			data.allItems.exp.forEach(function(cur){
+				cur.calcPercentages(data.totals.inc);
+			});
+
+		},
+
+		getPercentages: function(){
+			var allPerc = data.allItems.exp.map(function(cur){
+				return cur.getPercentage();
+			});
+			return allPerc;
 		},
 
 		getBudget: function() {
@@ -236,10 +264,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 	var updatePercentages = function() {
 
 		//1. Calculate percentages
+		budgetCtrl.calculatePercentages();
 
 		//2. Read from budget controller
+		var percentages = budgetCtrl.getPercentages();
 
 		//3. Update UI
+		console.log(percentages);
 
 	};
 
